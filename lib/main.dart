dcdd806f5d32fc3c03d856693d14b498ad3baa67
TalelMejri <talelmejri8@gmail.com>
2023-08-12 12:58:11 +0100

    rev ShowDialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:simpledialog/choiceList.dart';
 
-import 'SecondSecreen.dart';
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp( const HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DialogExempleAPP',
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'DialogExempleAPP'),
+      home: HomeApp(title:"Hello"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeApp extends StatefulWidget{
+  const HomeApp({super.key,required this.title});
+
   final String title;
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeApp> createState()=>_HomeApp();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeApp extends State<HomeApp>{
 
-  String choice="no choice yet";
-
-  void setChoice(String val){
-    setState(() {
-      this.choice=val;
-    });
-  }
-  
-Future<void> _navigateAndDisplaySelection(BuildContext context) async {
-  String? result = await Navigator.push(
-    context,
-    MaterialPageRoute(builder: (BuildContext context) => SecondScreen()),
-  );
-
-  if (result != null) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(result)),
-    );
-  }
-}
-
-  Future<void> pinkChoice() async{
-    ChoiceList ?resultat=await showDialog<ChoiceList>(barrierDismissible: false,context: context, builder:(BuildContext context){
-      return SimpleDialog(
-        title: Text("What Is Your Favorite mean of transport"),
-        children: [
-          SimpleDialogOption(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+   Future<void> choiceSomethin() async {
+      String ? result=await showDialog(barrierDismissible: false,context: context, builder: (context){
+            return SimpleDialog(
+              title: const Text("choice something"),
               children: [
-                Icon(Icons.flight),
-                Text("plane")
+                SimpleDialogOption(
+                  onPressed: (){Navigator.pop(context,"heart");},
+                  child: const Row(children: [
+                    Icon(Icons.favorite),
+                     Padding(padding: EdgeInsets.only(left: 10)),
+                    Text("Heart")
+                    ]),
+                ),
+                  SimpleDialogOption(
+                      onPressed: (){Navigator.pop(context,"Alarm");},
+                  child: const Row(children: [
+                    Icon(Icons.access_alarms),
+                     Padding(padding: EdgeInsets.only(left: 10)),
+                    Text("Alarm")
+                    ]),
+                ),
+                  SimpleDialogOption(
+                   onPressed: (){Navigator.pop(context,"home");},
+                  child:const Row(children: [
+                    Icon(Icons.home),
+                     Padding(padding: EdgeInsets.only(left: 10)),
+                    Text("home")
+                    ]),
+                )
               ],
-            ),
-            onPressed: (){Navigator.pop(context,ChoiceList.plane);},
-          ),
-          SimpleDialogOption(
-            child: Row(
-                 mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(Icons.directions_car),
-                Text("Car")
-              ],
-            ),
-            onPressed: (){Navigator.pop(context,ChoiceList.car);},
-          ),
-          SimpleDialogOption(
-            child: Row(
-                 mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(Icons.directions_boat),
-                Text("Boat")
-              ],
-            ),
-            onPressed: (){Navigator.pop(context,ChoiceList.boat);},
-          )
-        ],
-      );});
+            );
+      });
+      switch(result){
+        case 'home':
+        setState(() {
+           choice="home";
+        });
+         break;
+           case "Alarm":
+         setState(() {
+             choice="Alarm";
+         });
+       break;
+         case "heart":
+         setState(() {
+            choice="heart";
+         });
+         break;
+      }
+   }
 
-    if (resultat!=null){
-     switch (resultat) {
-      case ChoiceList.boat:
-        setChoice("boat");
-        break;
-      case ChoiceList.plane:
-        setChoice("plane");
-        break;
-      case ChoiceList.car:
-        setChoice("car");
-        break;
-    }
-    }
-  }
+  String choice="no yet";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ), 
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           ElevatedButton(onPressed:pinkChoice, child: Text("open Simple Dialog",style: TextStyle(color: Colors.white,fontSize: 20),),style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),),
-           Text("this You Choice "),
-           Text("${choice}",style: TextStyle(fontSize: 25)),
-           IconButton(onPressed:()async { await _navigateAndDisplaySelection(context); } , icon: Icon(Icons.replay))
-        ]),
+   return Scaffold(
+    appBar: AppBar(title: Text("Hello"),),
+    body: Center(child: 
+      Column(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
+         ElevatedButton(onPressed: choiceSomethin, child: 
+           Text("Choice Plan",style: TextStyle(fontSize:18),),style: ElevatedButton.styleFrom(backgroundColor: Colors.indigoAccent),),
+          const Padding(padding:EdgeInsets.only(bottom: 20)),
+          Text("${choice}",style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 25),)
+      ],
       ),
-    );
+      ),
+   );
   }
 }
---
+++ lib/main_officiel.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:simpledialog/choiceList.dart';
+
+import 'SecondSecreen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DialogExempleAPP',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'DialogExempleAPP'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String choice="no choice yet";
+
+  void setChoice(String val){
+    setState(() {
+      choice=val;
+    });
+  }
+  
+Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+  String? result = await Navigator.push(
+    context,
+    MaterialPageRoute(builder: (BuildContext context) => SecondScreen()),
+  );
+
+  if (result != null) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(result)),
+    );
+  }
+}
+
+  Future<void> pinkChoice() async{
+    ChoiceList ?resultat=await showDialog<ChoiceList>(
+      barrierDismissible: false,
+      context: context,
+       builder:(BuildContext context){
+      return SimpleDialog(
+        title: const Text("What Is Your Favorite mean of transport"),
+        children: [
+          SimpleDialogOption(
+            child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.flight),
+                Text("plane")
+              ],
+            ),
+            onPressed: (){Navigator.pop(context,ChoiceList.plane);},
+          ),
+          SimpleDialogOption(
+            child: const Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.directions_car),
+                Text("Car")
+              ],
+            ),
+            onPressed: (){Navigator.pop(context,ChoiceList.car);},
+          ),
+          SimpleDialogOption(
+            child: const Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.directions_boat),
+                Text("Boat")
+              ],
+            ),
+            onPressed: (){Navigator.pop(context,ChoiceList.boat);},
+          )
+        ],
+      );});
+
+    if (resultat!=null){
+     switch (resultat) {
+      case ChoiceList.boat:
+        setChoice("boat");
+        break;
+      case ChoiceList.plane:
+        setChoice("plane");
+        break;
+      case ChoiceList.car:
+        setChoice("car");
+        break;
+    }
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ), 
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           ElevatedButton(onPressed:pinkChoice,
+            child: 
+            Text("open Simple Dialog",
+            style: TextStyle(color: Colors.white,fontSize: 20),),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.teal),),
+           const Text("this You Choice "),
+           Text("${choice}",style: const TextStyle(fontSize: 25)),
+           IconButton(onPressed:()async { await _navigateAndDisplaySelection(context); } ,
+            icon: Icon(Icons.replay))
+        ]),
+      ),
+    );
+  }
+}
